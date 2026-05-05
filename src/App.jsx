78815eae4dbd55f8482ce035d91b405f5957549d
Chakshu <gnrage69@gmail.com>
2026-05-05 17:09:44 +0530

    Adding comments for better understanding

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header.jsx'
 import StudentTable from './components/StudentTable.jsx'
 import AddStudentForm from './components/AddStudentForm.jsx'
 
+// Initial student data
 const initialStudents = [
   { id: 1, name: 'Aarav Sharma', score: 78 },
   { id: 2, name: 'Priya Patel', score: 35 },
@@ -12,17 +13,29 @@ const initialStudents = [
 ]
 
 function App() {
+  // useState stores the students array in state
+  // 'students' is the current data, 'setStudents' is used to update it
+  // whenever setStudents is called, React re-renders the UI automatically
   const [students, setStudents] = useState(initialStudents)
 
+  // 'updateScore' updates the score of a specific student by their id
+  // 'id' - the unique id of the student to update
+  // 'newScore' - the new score value entered by the user
+
   const updateScore = (id, newScore) => {
-    setStudents(students.map(student =>
-      student.id === id
+    setStudents(students.map(student =>   // .map() loops through all the students
+      student.id === id         // if the student's id matches, we return a new object with updated score
         ? { ...student, score: Number(newScore) }
         : student
-    ))
+    ))    // if id doesn't match , we return the student unchanged
   }
 
+// addStudent - adds a brand new student to the students array
+// name - student name from the form input
+// score - student score from the form input
   const addStudent = (name, score) => {
+    // creates a new student object
+    // Date.now() generates a unique number based on current time which is used as id
     const newStudent = {
       id: Date.now(),
       name: name,
